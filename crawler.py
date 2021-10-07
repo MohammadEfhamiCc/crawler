a4cd429cf09f77030f93205cb2b2dc60cfa02ac9
Mohammad Efhami Sisi <efhamimohammad@gmail.com>
2021-10-07 12:17:28 +0330

    creating all the necessary funcitons to create database, create collection, check whether or not the database exist in MongoDb, and a function to insert the given url by program to MongoDB

--- crawler.py
+++ crawler.py
@@ -17,6 +17,7 @@ class cralwe:
         self.client=py.MongoClient(port=27017) 
         self.db_instance=self.client["urls"]
         self.col_instance=self.db_instance["urls_information"]
+        self.db_lists=self.client.list_database_names()
         self.exceptions=requests.exceptions.MissingSchema,\
         requests.exceptions.ConnectionError, requests.exceptions.InvalidURL,\
         requests.exceptions.InvalidSchema
@@ -25,6 +26,30 @@ class cralwe:
         self.urls_internal=set()
         self.urls_external=set()
         self.urls_failed=set()
+        self.document=list()
+        self.counter=0
 
+    def chech_db(self):
+        if "urls" in self.db_lists:
+            return True
+        else:
+            return False
+
+    def connect_to_db(self):
+        self.db_instance=self.client.urls
+        self.col_instance=self.db_instance.urls_information
+
+    def create_db(self):
+        self.db_instance=self.client["urls"]
+        self.col_instance=self.db_instance["urls_information"]
+
+    def insert_to_db(self,document):
+        self.col_instance.insert_one(self.document)
+
+
+
+
+
+        
 domain="www.google.com"
 init=cralwe(domain)
